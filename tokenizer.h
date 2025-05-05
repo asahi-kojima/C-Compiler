696f8aaffd94e77249a4ad3af599616645bdf64f
asahi-kojima-mac <none>
2025-05-05 23:35:13 +0900

    add : 機能に関するコメントを追加

--- tokenizer.h
+++ tokenizer.h
@@ -17,10 +17,14 @@ enum class TokenKind
 
 struct Token
 {
+    //トークンタイプ
     TokenKind token_kind;
 
+    //このトークンの入力コード内での場所
+    //入力コード列をp,先頭からNbyte目からトークン文字列が始まっていればp+Nが入る
     const char* token_position_in_input_string;
 
+    //トークンタイプ毎にプロパティが必要になるので、それを格納しておくユニオン型変数
     union TokenProperty
     {
         struct OfNum {s32 value;} of_num;
@@ -38,26 +42,35 @@ class TokenStream
 public:
     TokenStream(char* input_string);
 
+    //現在のトークンタイプがRESERVEDであり、かつ指定した文字列の場合にトークンを読み進める。
     bool consume_if(const char* str);
-    bool consume_if(TokenKind kind);
+
+    //トークンがRESERVED型であり、指定した文字列である事を確認する。
     void expect(const char* str);
+
+    //現在のトークンが数字である事を確認する。
     s32 expect_number();
 
     Token get_current_token() const;
+
+    //トークン列が終了か判定する。
     bool at_end() const;
 
+    //デバッグ関数
     void print_all_tokens() const;
 
 
 private:
+    //トークン分割を行う関数
     void tokenize();
 
+    //分割したトークンを格納するリスト
     std::vector<Token> m_token_list;
-    //u32 m_current_token_index;
 
+    //トークン列を指すイテレーターと終端イテレーター
     std::vector<Token>::iterator m_current_token_iter;
     std::vector<Token>::iterator m_end_token_iter;
 
+    //入力されたコードを保存しておく変数：エラー表示に利用
     char* m_input_string = nullptr;
-
 };
